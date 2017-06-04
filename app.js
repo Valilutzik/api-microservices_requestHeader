c8ea59c90148f90a7047c5fcf51f87dbbd76e11a
Valilutzik <w.ragheb90@gmail.com>
2017-06-04 17:01:22 +0000

    configured port number

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ var http = require("http");
 var express = require("express");
 
 var app = express();
+var port = process.env.PORT || 8080;
 
 app.use(function(req, res)
         {
             res.json({"Ip address":req.ip, "UserAgent":req.get("user-agent"), "language":req.acceptsLanguages()});
         })
 
-http.createServer(app).listen(3020);
+http.createServer(app).listen(port);
