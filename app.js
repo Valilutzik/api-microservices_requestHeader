2713fca06f227e361871b0f9ee9c38cfca3e77c4
Valilutzik <w.ragheb90@gmail.com>
2017-06-04 17:45:14 +0000

    added ip middleware

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 var http = require("http");
 var express = require("express");
+const requestIp = require('request-ip');
 
 var app = express();
 var port = process.env.PORT || 8080;
+
+//middleware handler 
+const ipMiddleware = function(req, res, next) {
+    const clientIp = requestIp.getClientIp(req); 
+    req.ip = clientIp;
+    next();
+};
+
+app.use(ipMiddleware);
 
 app.use(function(req, res)
         {
